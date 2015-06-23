72fb5d189fca2bbe9cd136d79568947008747a84
gitelian <gitelian@berkeley.edu>
2015-06-23 14:11:58 -0700

    Will create a .dat file with running encoder data and stimulus information

--- create_dat_file.m
+++ create_dat_file.m
@@ -1,34 +1,33 @@
+%CREATE_DAT_FILE creates a .dat file that contains a matrix with running
+%    encoder values, stimulus sequence info, and other basic values saved in
+%    the MCdata .phy file with the exception of the actual neural data. This
+%    information will be used by the whisker tracking software for identifying
+%    running trials and the stimulus presented during those trials.
 
+function create_dat_file(mcdata_dir, mcdata_file)
 
-
-
-function create_dat_file(mcdata_file, varargin)
-
-if nargin == 1
-    mcdata_dir = '~/Documents/AdesnikLab/Data';
-elseif nargin == 2
-    mcdata_dir = varargin{1};
     if exist(mcdata_dir,'dir') == 0
         error('Directory does not exist')
-    end 
+    end
+
+    if exist([mcdata_dir filesep mcdata_file],'file') == 0
+        error('File does not exist')
+    end
+
+    disp('loading MCdata file')
+    load([mcdata_dir filesep mcdata_file],'-mat')
+
+    %% Save Running File
+    run_data = zeros(length(MCdata{1,1}(:,end)),length(MCdata));
+    disp('making running file')
+    for k = 1:length(MCdata);
+        run_data(:,k) = MCdata{1,k}(:,end);
+    end
+
+    save([mcdata_dir filesep mcdata_file(1:end-4) '.dat'],'run_data','time',...
+        'aoFinal','stimsequence','ao_latency','ao_interval','ao_duration',...
+        'ao_amplitude','ao_quantity','optosquare','optoramp','-v7.3');
+
+    disp(['Saved: ' mcdata_dir filesep mcdata_file(1:end-4) '.dat'])
+
 end
-disp([mcdata_dir filesep mcdata_file])
-if exist([mcdata_dir filesep mcdata_file],'file') == 0
-    error('File does not exist')
-end
-
-tic()
-
-disp('loading MCdata file')
-load([mcdata_dir filesep mcdata_file],'-mat')
-
-mcdata_temp = cell(size(MCdata));
-
-%% Save Running File
-run_data = zeros(length(MCdata{1,1}(:,end)),length(MCdata));
-disp('making running file')
-for k = 1:length(MCdata);
-    run_data(:,k) = MCdata{1,k}(:,end);
-end
-save([mcdata_dir filesep mcdata_file(1:end-4) '.dat'],'run_data','time','aoFinal','stimsequence','ao_latency','ao_interval',...
-    'ao_duration','ao_amplitude','ao_quantity','optosquare','optoramp','-v7.3');
