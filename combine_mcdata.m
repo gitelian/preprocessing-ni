b65429b18594e9d51f5b0e3279a263dcb33788b1
gitelian <gitelian@berkeley.edu>
2015-08-17 19:08:30 -0700

    up to date whisker tracking analysis code. set-point vs runspeed, set-point and amplitude tuning curves, difference tuning curves

--- combine_mcdata.m
+++ combine_mcdata.m
@@ -1,57 +1,64 @@
 
+%%COMBINE_MCDATA.M combines MCdata .phy files.
+% Can only combine two whisker trimming files and CORRECTLY change the stim
+% indices for the second file.
+%
+% Can combine any number of non-whisker trimming MCdata files.
+%
+% example: combine_mcdata(0) (this is for NO whisker trimming
 
-% example: combine_mcdata({'0980', '0981'}, '/media/greg/data/Neuro/')
+function combine_mcdata(whisker_trim)
 
-function combine_mcdata(fids,varargin)
+disp('Only 2 files can be combined for a WHISKER TRIMMING EXPERIMENT!!!')
 
 if nargin > 1
-   data_path = varargin{1};
-   if exist(data_path,'dir') == 0
-       error('path doesn''t exist')
-   end
-else
-    data_path = '~/Documents/AdesnikLab/Data';
+    error('Whisker Trimming Argument Not Specified!!!')
 end
 
-num_fids = length(fids);
+[file_names, path_name] = uigetfile(...
+                                    {'*.phy', 'Phys Files';...
+                                    '*.*', 'All Files (*.*)'},...
+                                    'Select .phy files to combine',...
+                                    'MultiSelect', 'on',...
+                                    '/media/greg/Data/Neuro/');
 
-for k = 1:num_fids
-    dir_strct = dir([data_path filesep '*' fids{k} '*.phy']);
-    if isempty(dir_strct)
-        error(['file not found (fid: ' num2str(fids{k}) ')'])
-    end
-end
+num_files = length(file_names);
 
 fname_head = [];
 
-for k = 1:num_fids
-    
+for k = 1:num_files
+
     if k == 1
-        dir_strct = dir([data_path filesep '*' fids{k} '*.phy']);
-        disp(['loading: ' dir_strct.name])
-        x = load([data_path filesep dir_strct.name],'-mat'); % memory explodes on second load have no idea why
+        current_file = [path_name file_names{k}];
+        disp(['loading: ' current_file])
+        x = load(current_file, '-mat'); % memory explodes on second load have no idea why
         trial_length = length(x.MCdata{1,1}(:,1));
         disp('Done loading')
-        fname_head = [fname_head,dir_strct.name(1:4)];
+        fname_head = [fname_head,file_names{k}(1:4)];
     elseif k > 1
-        dir_strct = dir([data_path filesep '*' fids{k} '*.phy']);
-        disp(['loading: ' dir_strct.name])
-        y = load([data_path filesep dir_strct.name],'-mat'); % memory explodes on second load have no idea why
+        current_file = [path_name file_names{k}];
+        disp(['loading: ' current_file])
+        y = load(current_file, '-mat'); % memory explodes on second load have no idea why
         disp('Done loading')
-        %fname_head = dir_strct.name(1:4);        
-        fname_head = [fname_head '-' dir_strct.name(1:4)];
+        %fname_head = dir_strct.name(1:4);
+        fname_head = [fname_head '-' file_names{k}(1:4)];
        if trial_length ~= length(x.MCdata{1,1}(:,1));
            warning('Trial lengths are not the same length')
        end
-       
+
        x.MCdata = [x.MCdata,y.MCdata];
-       x.stimsequence = [x.stimsequence,y.stimsequence];
-       
-    end   
+       if whisker_trim == 0
+           x.stimsequence = [x.stimsequence,y.stimsequence];
+       elseif whisker_trim == 1
+           disp('changing stimsequence for whisker trimming')
+           x.stimsequence = [x.stimsequence,y.stimsequence+(length(unique(y.stimsequence)))];
+       end
+
+    end
 end
 
-disp(['Saving: ' fname_head dir_strct.name(5:end)])
-save([data_path filesep fname_head dir_strct.name(5:end)],'-struct','x','-v7.3')
+disp(['Saving: ' fname_head file_names{k}(5:end)])
+save([path_name filesep fname_head file_names{k}(5:end)],'-struct','x','-v7.3')
 
 
 
--- split_multielectrode_data.m
+++ split_multielectrode_data.m
@@ -9,57 +9,27 @@
 % Adesnik Lab
 % UC Berkeley
 % 20140121
-%
-% 20150623 GT: Minor cosmetic/syntactic updates and added functionality to
-% update the stimsequence vector for a whisker trimming experiment.
 
-function split_multielectrode_data(mcdata_file, varargin)
 
-if nargin == 1
-    mcdata_dir = '~/Documents/AdesnikLab/Data';%Change this for other computer!!!
-    whisker_trim_bool = 0;
+function split_multielectrode_data()
 
-elseif nargin == 2
-
-    mcdata_dir = varargin{1};
-    if exist(mcdata_dir,'dir') == 0
-        error('Directory does not exist')
-    end
-
-    whisker_trim_bool = 0;
-
-elseif nargin == 3
-
-    mcdata_dir = varargin{1};
-    if exist(mcdata_dir,'dir') == 0
-        error('Directory does not exist')
-    end
-
-    whisker_trim_bool = varargin{2};
-    if whisker_trim_bool
-        disp(['Whisker trimming file! Updating stimsequence labels'])
-    else
-        disp(['Not a whisker trimming file. Proceeding with regular split'])
-    end
-
-end
-
-disp([mcdata_dir filesep mcdata_file])
-if exist([mcdata_dir filesep mcdata_file],'file') == 0
-    error('File does not exist')
-end
+disp('ONLY A FILE FROM 2 16 CHANNEL RECORDINGS CAN BE SPLIT!!!')
+[file_name, path_name] = uigetfile(...
+                                    '*.phy',...
+                                    'Select .phy file to split',...
+                                    '/media/greg/Data/Neuro/');
 
 tic()
 
 disp('loading MCdata file')
-load([mcdata_dir filesep mcdata_file],'-mat')
+load([path_name file_name],'-mat')
 
 disp(['MCdata size:' num2str(size(MCdata,1)) ' by ' num2str(size(MCdata),2)]);
 disp(['MCdata trial size:' num2str(size(MCdata{1},1)) ' by ' num2str(size(MCdata{1},2))]);
 mcdata_temp = cell(size(MCdata));
 
 disp('checking size of MCdata file')
-if size(MCdata{1},2) ~= 33 && size(MCdata{1},2) ~= 35
+if size(MCdata{1},2) ~= 33
     error('Original MCdata file is not the expected size')
 end
 
@@ -70,12 +40,7 @@ for k = 1:length(MCdata);
     run_data(:,k) = MCdata{1,k}(:,end);
 end
 
-split_index = length(stimsequence)/2;
-if whisker_trim_bool && mod(split_index, 2) == 0
-    stimsequence = [stimsequence(1:split_index), stimsequence(split_index+1:end)+7];
-end
-
-save([mcdata_dir filesep mcdata_file(1:end-4) '.dat'],'run_data','time',...
+save([path_name file_name(1:end-4) '.dat'],'run_data','time',...
     'aoFinal','stimsequence','ao_latency','ao_interval','ao_duration',...
     'ao_amplitude','ao_quantity','optosquare','optoramp','-v7.3');
 
@@ -90,29 +55,20 @@ clear MCdata
 MCdata = mcdata_temp;
 clear mcdata_temp;
 
-split_index = length(stimsequence)/2;
-if whisker_trim_bool && mod(split_index, 2) == 0
-    stimsequence = [stimsequence(1:split_index), stimsequence(split_index+1:end)+7];
-end
-
-save([mcdata_dir filesep mcdata_file(1:end-4) '_e1.phy'],'MCdata','time',...
+save([path_name file_name(1:end-4) '_e1.phy'],'MCdata','time',...
     'aoFinal','stimsequence','ao_latency','ao_interval','ao_duration',...
     'ao_amplitude','ao_quantity','optosquare','optoramp','-v7.3');
-save([tempdir filesep 'temp_usr.mat'],'mcdata_file','mcdata_dir','whisker_trim_bool','-v7.3');
+save([tempdir filesep 'temp_usr.mat'],'file_name','path_name','-v7.3');
 
 disp('clearing memory')
 clear all
-cwd = pwd;
-cd(tempdir);
-pack
-cd(cwd)
 
 disp(' ')
 
 %% Save Last 16 Channels to e2.phy file
 disp('loading MCdata file')
 load([tempdir filesep 'temp_usr.mat'])
-load([mcdata_dir filesep mcdata_file ],'-mat')
+load([path_name file_name ],'-mat')
 
 mcdata_temp = cell(size(MCdata));
 
@@ -126,22 +82,12 @@ clear MCdata
 MCdata = mcdata_temp;
 clear mcdata_temp
 
-split_index = length(stimsequence)/2;
-if whisker_trim_bool && mod(split_index, 2) == 0
-    stimsequence = [stimsequence(1:split_index), stimsequence(split_index+1:end)+7];
-end
-
-save([mcdata_dir filesep mcdata_file(1:end-4) '_e2.phy'],'MCdata','time',...
+save([path_name filesep file_name(1:end-4) '_e2.phy'],'MCdata','time',...
     'aoFinal','stimsequence','ao_latency','ao_interval','ao_duration',...
     'ao_amplitude','ao_quantity','optosquare','optoramp','-v7.3');
 
 disp('clearing memory')
 clear all
-cwd = pwd;
-cd(tempdir);
-pack
-cd(cwd)
-
 
 delete([tempdir filesep 'temp_usr.mat'])
 
